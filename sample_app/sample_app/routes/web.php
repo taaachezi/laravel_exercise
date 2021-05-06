d31c935789d35b74b1669400c56209f217c85410
taaachezi <64632134+taaachezi@users.noreply.github.com>
2021-05-07 08:51:49 +0900

    install_auth

--- sample_app/sample_app/routes/web.php
+++ sample_app/sample_app/routes/web.php
@@ -18,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::get("/","PlayersController@index");
 Route::get("/index","PlayersController@index");
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
