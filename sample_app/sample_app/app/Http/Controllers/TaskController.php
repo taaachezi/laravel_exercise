077f6ddf9bf2a2125310e46d64b1375bf1d96101
taaachezi <64632134+taaachezi@users.noreply.github.com>
2021-05-12 23:30:21 +0900

    todo_app

--- sample_app/sample_app/app/Http/Controllers/TaskController.php
+++ sample_app/sample_app/app/Http/Controllers/TaskController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index()
     {
-    	return "Hello world";
+    	$folders = Folder::all();
+    	return view("tasks/index", ["folders"=>$folders,]);
     }
 }
--- sample_app/sample_app/routes/web.php
+++ sample_app/sample_app/routes/web.php
@@ -12,18 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 // Route::get("/","PlayersController@index");
 // Route::get("/index","PlayersController@index");
-
 // Auth::routes();
-
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Auth::routes();
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get("/folders/{id}/tasks", "TaskController@index")->name("tasks.index");
