d2d8136b568423a35db467dddd1f2cb30431b42d
taaachezi <64632134+taaachezi@users.noreply.github.com>
2021-05-16 14:12:38 +0900

    phpunit_task_test

--- sample_app/sample_app/routes/web.php
+++ sample_app/sample_app/routes/web.php
@@ -23,3 +23,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get("/folders/{id}/tasks", "TaskController@index")->name("tasks.index");
+Route::get("/folders/create", "FolderController@showCreateForm")->name("folders.create");
+Route::post("/folders/create", "FolderController@create");
+Route::get("/folders/{id}/tasks/create", "TaskController@showCreateForm")->name("tasks.create");
+Route::post("folders/{id}/tasks/create", "TaskController@create");
